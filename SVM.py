d4f159da54f954b74ea8222b6945ec1b8d86b250
VXSONG <VXSONG@126.com>
2023-04-29 01:05:46 +0800

    test

--- SVM.py
+++ SVM.py
@@ -6,6 +6,7 @@
 # 2023/4/13
 
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -53,5 +54,61 @@ plt.title('Confusion Matrix(wdbc.data)')
 plt.show()
 
 ################### 数据集winequality.data##############################
-df2=pd.read_csv('./dataset/winequality/winequality-red.csv',header=None)
+# 加载数据集
+df2 = pd.read_csv('./dataset/winequality/winequality-red.csv', delimiter=';')
+
+X = df2.iloc[:, :-1].values
+y = df2.iloc[:, -1].values
+
+# 划分训练集和测试集
+X_train2, X_test2, y_train2, y_test2 = train_test_split(X, y, test_size=0.2)
+# 训练SVM模型
+model = SVC(kernel='linear')
+model.fit(X_train2, y_train2)
+
+# 预测测试集类别
+y_pred2 = model.predict(X_test2)
+
+# 计算模型的评价指标
+accuracy2 = accuracy_score(y_test2, y_pred2)
+precision2 = precision_score(y_test2, y_pred2, average='macro', zero_division=1)
+recall2 = recall_score(y_test2, y_pred2, average='macro')
+f1_2_1 = f1_score(y_test2, y_pred2, average='micro')
+f1_2_2 = f1_score(y_test2, y_pred2, average='macro')
+
+print("模型准确率：", accuracy2)
+print("模型精确率：", precision2)
+print("模型召回率：", recall2)
+print("模型 F1 值(micro)：", f1_2_1)
+print("模型 F1 值(macro)：", f1_2_2)
+
+# 绘制混淆矩阵
+cm = confusion_matrix(y_test2, y_pred2)
+# 生成混淆矩阵热图
+plt.figure(figsize=(8, 6))
+sns.heatmap(cm, annot=True, cmap='Blues')
+plt.xlabel('Predicted Labels')
+plt.ylabel('True Labels')
+plt.title('Confusion Matrix(winequality-red.csv)')
+plt.show()
+
+
+# 绘制柱状图
+fig, ax = plt.subplots()
+index = np.arange(5)
+bar_width = 0.35
+opacity = 0.8
+
+list1 = [accuracy, precision, recall, f1_1_1, f1_1_2]
+list2 = [accuracy2, precision2, recall2, f1_2_1, f1_2_2]
+rects1 = ax.bar(index, list1, bar_width, alpha=opacity, color='b', label='wdbc')
+rects2 = ax.bar(index + bar_width, list2, bar_width, alpha=opacity, color='g', label='winequality-red')
+
+ax.set_xlabel('Iteration')
+ax.set_ylabel('Error')
+ax.set_title('Evaluation Indicators')
+ax.set_xticks(index + bar_width / 2)
+ax.set_xticklabels(('1', '2', '3', '4', '5'))
+ax.legend()
+plt.show()
 
--- test.py
+++ test.py
@@ -5,7 +5,7 @@
 # SONG
 # 2023/4/13
 
-#ValueError: could not convert string to float: 'fixed acidity'
+# ValueError: could not convert string to float: 'fixed acidity'
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -13,66 +13,5 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
-
-# 加载数据集
-df2=pd.read_csv('./dataset/winequality/winequality-red.csv',delimiter=';')
-
-X = df2.iloc[:, :-1]
-y = df2.iloc[:, -1]
-print(X,y)
-
-# 划分训练集和测试集
-X_train, X_test, y_train, y_test = train_test_split(X.values, y.values, test_size=0.2, random_state=42)
-
-
-print('开始训练')
-# 训练SVM模型
-model = SVC(kernel='linear', C=1)
-
-model.fit(X_train, y_train)
-print('模型训练结束')
-
-# 预测测试集类别
-y_pred = model.predict(X_test)
-
-model.fit(X_train2, y_train2)
-print('模型训练结束')
-
-# 预测测试集类别
-y_pred2 = model.predict(X_test2)
-
-print('预测结束')
-
-
-# 计算模型的评价指标
-
-accuracy2 = accuracy_score(y_test, y_pred)
-precision2 = precision_score(y_test, y_pred)
-recall2 = recall_score(y_test, y_pred)
-f1_2_1 = f1_score(y_test, y_pred, average='micro')
-f1_2_2 = f1_score(y_test, y_pred, average='macro')
-
-accuracy2 = accuracy_score(y_test2, y_pred2)
-precision2 = precision_score(y_test2, y_pred2)
-recall2 = recall_score(y_test2, y_pred2)
-f1_2_1 = f1_score(y_test2, y_pred2, average='micro')
-f1_2_2 = f1_score(y_test2, y_pred2, average='macro')
-
-print("模型准确率：", accuracy2)
-print("模型精确率：", precision2)
-print("模型召回率：", recall2)
-print("模型 F1 值(micro)：", f1_2_1)
-print("模型 F1 值(macro)：", f1_2_2)
-
-# 绘制混淆矩阵
-cm = confusion_matrix(y_test2, y_pred2)
-# 生成混淆矩阵热图
-plt.figure(figsize=(8, 6))
-sns.heatmap(cm, annot=True, cmap='Blues')
-plt.xlabel('Predicted Labels')
-plt.ylabel('True Labels')
-plt.title('Confusion Matrix(wdbc.data)')
-plt.show()
-
 
 
