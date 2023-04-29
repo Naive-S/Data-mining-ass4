7bf3221ccca99fcc5ac95247adac5e9970c6d6cd
VXSONG <VXSONG@126.com>
2023-04-29 08:23:18 +0800

    test

--- 神经网络.py
+++ 神经网络.py
@@ -1,11 +1,18 @@
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
 
-# 加载数据集
-df2=pd.read_csv('./dataset/winequality/winequality-red.csv',delimiter=';')
-print(df2.head())
 
+# 加载数据集
+df = pd.read_csv('./dataset/wdbc/wdbc.data', header=None)
+
+# 去除第一列ID以及第二列标签列
+X = df.iloc[:, 2:]
+y = df.iloc[:, 1].apply(lambda x: 1 if x == 'B' else 0)
+print(X)
+
+# 划分训练集和测试集
+X_train, X_test, y_train, y_test = train_test_split(X.values, y.values, test_size=0.2, random_state=42)
