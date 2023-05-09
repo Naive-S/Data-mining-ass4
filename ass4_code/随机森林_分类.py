3c7c10e8c63830af33f5ae522a1449f86b07e999
VXSONG <VXSONG@126.com>
2023-05-09 16:03:28 +0800

    not complete

--- ass4_code/随机森林_分类.py
+++ ass4_code/随机森林_分类.py
@@ -7,3 +7,30 @@
 """
 # SONG
 # 2023/5/9
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+df = pd.read_csv('../dataset/iris/iris.data', header=None)
+
+data = df.iloc[:, :-1]
+target = df.iloc[:, -1]
+
+# 数据预处理
+target = target.replace({'Iris-setosa': 0, 'Iris-versicolor': 1, 'Iris-virginica': 2})
+
+# 划分训练集和测试集
+X_train, X_test, y_train, y_test = train_test_split(data.values, target.values, test_size=0.25)
+# 创建随机森林模型，使用10棵决策树，每棵树的最大深度为2
+rf = RandomForestClassifier(n_estimators=10, max_depth=2)
+
+# 训练模型
+rf.fit(X_train, y_train)
+
+# 预测测试集结果
+y_pred = rf.predict(X_test)
+
+# 计算准确率
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
