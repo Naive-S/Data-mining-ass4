f5e582db89adae2e889c7e3d6e1afa989c34eba8
VXSONG <VXSONG@126.com>
2023-05-11 18:48:09 +0800

    Finish

--- ass4_code/IRIS_分类.py
+++ ass4_code/IRIS_分类.py
@@ -9,10 +9,11 @@
 # 2023/5/9
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.tree import DecisionTreeClassifier
+import matplotlib.pyplot as plt
 
-################### 随机森林 ###############################
 df = pd.read_csv('../dataset/iris/iris.data', header=None)
 
 data = df.iloc[:, :-1]
@@ -22,9 +23,12 @@ target = df.iloc[:, -1]
 target = target.replace({'Iris-setosa': 0, 'Iris-versicolor': 1, 'Iris-virginica': 2})
 
 # 划分训练集和测试集
-X_train, X_test, y_train, y_test = train_test_split(data.values, target.values, test_size=0.25,random_state=42)
-# 创建随机森林模型，使用10棵决策树，每棵树的最大深度为2
-rf = RandomForestClassifier(n_estimators=10, max_depth=4)
+X_train, X_test, y_train, y_test = train_test_split(data.values, target.values, test_size=0.25, random_state=42)
+
+####################################### 随机森林 #########################################################
+
+# 创建随机森林模型，使用100棵决策树，每棵树的最大深度为4
+rf = RandomForestClassifier(n_estimators=100, max_depth=4)
 
 # 训练模型
 rf.fit(X_train, y_train)
@@ -38,12 +42,69 @@ pre_re_f1 = precision_score(y_test, y_pred, average='micro')
 precision = precision_score(y_test, y_pred, average='macro')
 recall = recall_score(y_test, y_pred, average='macro')
 f1 = f1_score(y_test, y_pred, average='macro')
-# 打印评价指标
-print("随机森林分类效果：")
-print("accuracy:", accuracy)
-print("precision_macro:", precision)
-print("recall_macro:", recall)
-print("f1_macro:", f1)
-print("pre_re_d1_micro:",pre_re_f1)
+
+##################################### Adaboost（决策树基分类器） ##############################################
+# 构建AdaBoost分类器
+ada = AdaBoostClassifier(
+    estimator=DecisionTreeClassifier(max_depth=1),
+    n_estimators=200,
+    learning_rate=0.5
+)
+
+# 训练分类器
+ada.fit(X_train, y_train)
+
+# 预测测试集标签
+y_pred2 = ada.predict(X_test)
+
+# 计算评价指标
+accuracy2 = accuracy_score(y_test, y_pred2)
+pre_re_f12 = precision_score(y_test, y_pred2, average='micro')
+precision2 = precision_score(y_test, y_pred2, average='macro')
+recall2 = recall_score(y_test, y_pred2, average='macro')
+f12 = f1_score(y_test, y_pred2, average='macro')
+
+# 定义评价指标名称
+metrics = ['Accuracy', 'Precision (macro)', 'Recall (macro)', 'F1-score (macro)', 'Pre_Rec_F1(micro)']
+
+# 提取随机森林和Adaboost分类器的评价指标
+rf_scores = [accuracy, precision, recall, f1, pre_re_f1]
+ada_scores = [accuracy2, precision2, recall2, f12, pre_re_f12]
+
+# 创建柱状图
+fig, ax = plt.subplots(figsize=(8, 5))
+bar_width = 0.35
+opacity = 0.8
+index = list(range(len(metrics)))
+rects1 = ax.bar(index, rf_scores, bar_width, alpha=opacity, color='b', label='Random Forest')
+rects2 = ax.bar([i + bar_width for i in index], ada_scores, bar_width, alpha=opacity, color='r', label='AdaBoost')
+
+# 添加轴标签和标题
+ax.set_xlabel('Metrics')
+ax.set_ylabel('Score')
+ax.set_title('Comparison of Random Forest and AdaBoost on IRIS dataset')
 
 
+# 添加数据标签
+def add_labels(rects):
+    for rect in rects:
+        height = rect.get_height()
+        ax.annotate(f'{height:.2f}',
+                    xy=(rect.get_x() + rect.get_width() / 2, height),
+                    xytext=(0, 3),
+                    textcoords="offset points",
+                    ha='center', va='bottom')
+
+add_labels(rects1)
+add_labels(rects2)
+# 添加轴标签和标题
+ax.set_xticks([i + bar_width / 2 for i in index])
+ax.set_xticklabels(metrics)
+plt.ylim([0.5, 1.2])
+
+# 添加图例
+ax.legend()
+
+# 显示图形
+plt.tight_layout()
+plt.show()
