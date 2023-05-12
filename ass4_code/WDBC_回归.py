70189942f644a6b881390a68531a432dd5d6d8df
VXSONG <VXSONG@126.com>
2023-05-12 21:50:38 +0800

    Finish

--- ass4_code/WDBC_回归.py
+++ ass4_code/WDBC_回归.py
@@ -10,6 +10,8 @@
 
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import  seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error, r2_score, explained_variance_score, median_absolute_error
 from sklearn.ensemble import RandomForestRegressor
@@ -29,7 +31,7 @@ X = data.drop(['id', 'diagnosis','fractal_dimension_worst'], axis=1)
 y = data['fractal_dimension_worst']
 
 # 将数据集划分为训练集和测试集
-X_trainval, X_test, y_trainval, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 ######################################### 随机森林回归器 ######################################################
 
@@ -37,7 +39,7 @@ X_trainval, X_test, y_trainval, y_test = train_test_split(X, y, test_size=0.2, r
 rf_regressor = RandomForestRegressor(n_estimators=100, random_state=42)
 
 # 训练模型
-rf_regressor.fit(X_trainval, y_trainval)
+rf_regressor.fit(X_train, y_train)
 
 # 预测测试集结果
 y_pred = rf_regressor.predict(X_test)
@@ -64,3 +66,56 @@ print('解释方差分数:', evs)
 medae = median_absolute_error(y_test, y_pred)
 print('中位绝对误差（MedAE）:', medae)
 
+
+######################################## Adaboost ###############################################
+from sklearn.ensemble import AdaBoostRegressor
+
+# 创建AdaBoost回归器，并以决策树为基本估计器
+ada_regressor = AdaBoostRegressor(n_estimators=50, random_state=42)
+
+# 使用训练数据拟合模型
+ada_regressor.fit(X_train, y_train)
+
+# 在测试数据上进行预测
+y_pred2 = ada_regressor.predict(X_test)
+
+print("\nAdaboost算法")
+# 计算均方误差（MSE）
+mse2 = np.mean((y_test - y_pred2) ** 2)
+print('均方误差（MSE）:', mse2)
+
+# 计算平均绝对误差（MAE）
+mae2 = mean_absolute_error(y_test, y_pred2)
+print('平均绝对误差（MAE）:', mae2)
+
+# 计算决定系数（R²）
+r22 = r2_score(y_test, y_pred2)
+print('决定系数（R²）:', r22)
+
+# 计算解释方差分数
+evs2 = explained_variance_score(y_test, y_pred2)
+print('解释方差分数:', evs2)
+
+# 计算中位绝对误差
+medae2 = median_absolute_error(y_test, y_pred2)
+print('中位绝对误差（MedAE）:', medae2)
+
+
+
+################################################ 绘图 ##############################################
+# 将评价指标绘制在一张图上进行对比
+fig, ax = plt.subplots(figsize=(10, 6))
+
+# 绘制随机森林算法的评价指标
+sns.lineplot(x=['MSE', 'MAE', 'R^2', 'Explained Variance', 'MedAE'], y=[mse, mae, r2, evs, medae], label='Random Forest', ax=ax)
+
+# 绘制Adaboost算法的评价指标
+sns.lineplot(x=['MSE', 'MAE', 'R^2', 'Explained Variance', 'MedAE'], y=[mse2, mae2, r22, evs2, medae2], label='Adaboost', ax=ax)
+
+# 设置图形标题、轴标签和图例
+ax.set_title('Comparison of Evaluation Metrics for Random Forest and Adaboost Algorithms')
+ax.set_xlabel('Evaluation Metrics')
+ax.set_ylabel('Value')
+ax.legend()
+
+plt.show()
